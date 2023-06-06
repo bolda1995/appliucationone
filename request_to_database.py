b7996c0810575c507194d36fcfd87b816c059c8c
bolda1995 <olegboldyrev2@gmail.com>
2023-06-06 17:15:37 +0300

    update

--- request_to_database.py
+++ request_to_database.py
@@ -9,7 +9,13 @@ class RequestTODataBase:
     )
 
     def insert_value(self, list_val: list):
-
+        conn = psycopg2.connect(
+            host="127.0.0.1",
+            port="5432",
+            database="onec_cinfo",
+            user="oleg",
+            password="Zxcv7890"
+        )
         sql_query = """
             INSERT INTO message_data (sending_process_status,
              need_rewrite, 
@@ -21,23 +27,30 @@ class RequestTODataBase:
              )
             VALUES (%s, %s, %s, %s, %s, %s, %s)
         """
-        cursor = self.conn.cursor()
+        cursor = conn.cursor()
         values = self.set_elements_for_db(list_val)
         cursor.execute(sql_query, values)
-        self.conn.commit()
+        conn.commit()
         cursor.close()
-        self.conn.close()
+        conn.close()
 
     def request_select(self):
+        conn = psycopg2.connect(
+            host="127.0.0.1",
+            port="5432",
+            database="onec_cinfo",
+            user="oleg",
+            password="Zxcv7890"
+        )
         sql_query = "SELECT * FROM message_data"
-        cursor = self.conn.cursor()
+        cursor = conn.cursor()
         cursor.execute(sql_query)
         rows = cursor.fetchall()
         list_rows = []
         for row in rows:
             list_rows.append(row)
         cursor.close()
-        self.conn.close()
+        conn.close()
         return list_rows
 
     def set_elements_for_db(self, list_val: list):
