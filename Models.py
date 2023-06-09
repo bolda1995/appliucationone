a8cd33f6f92c9233723a713f980cd0da1b19ae3e
bolda1995 <olegboldyrev2@gmail.com>
2023-06-09 09:07:44 +0300

    add column
    add column

--- Models.py
+++ Models.py
@@ -1,19 +1,2 @@
-from pydantic import BaseModel
 
-
-class Header(BaseModel):
-    need_rewrite: str
-    sending_process_status: str
-    message_type: str
-    processing_type: str
-    receiver_system: str
-    message_id: str
-    sender_system: str
-
-class MessageData(BaseModel):
-    data: str
-
-class Message(BaseModel):
-    header: Header
-    message: MessageData
 
---
+++ database/addcolumn.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+# Establish a connection to the database
+conn = psycopg2.connect(
+    host="127.0.0.1",
+    port="5432",
+    database="onec_cinfo",
+    user="oleg",
+    password="Zxcv7890"
+)
+
+# Create a cursor object to execute SQL queries
+cursor = conn.cursor()
+
+# Execute the ALTER TABLE query to add a new column
+alter_query = "ALTER TABLE message_data ADD COLUMN data TYPE VARCHAR(255)"
+cursor.execute(alter_query)
+
+# Commit the changes
+conn.commit()
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
+
+
+
+
+
+
