520eb823d51cad9ccb913ca494200e4eb2bbb22d
bolda1995 <olegboldyrev2@gmail.com>
2023-06-16 16:45:42 +0300

    update

--- getdata.py
+++ getdata.py
@@ -16,6 +16,6 @@ class GetData:
     def get_list_data(self, list_data: list):
         arr_message_id = []
         for iter_dict in list_data:
-            if iter_dict["status"] == "success":
-                arr_message_id.append(iter_dict["message-id"])
+            if iter_dict[0] == "success":
+                arr_message_id.append(iter_dict[0])
         return arr_message_id
